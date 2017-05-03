bc81019fb847bce303e95e726fd670dcb657815b
Michael Zellner <zellnerm@in.tum.de>
2017-05-03 11:20:48 +0200

    Modified profile_data

--- src/parser/parser_session_component.cc
+++ src/parser/parser_session_component.cc
@@ -60,18 +60,59 @@ Genode::Ram_dataspace_capability Parser_session_component::live_data()
 	return _live_data.cap();
 }
 
+
 Genode::Ram_dataspace_capability Parser_session_component::profile_data()
 {
+	mon_ds_cap = Genode::env()->ram_session()->alloc(100*sizeof(Mon_manager::Monitoring_object));
+	Mon_manager::Monitoring_object *threads=Genode::env()->rm_session()->attach(mon_ds_cap);
+	_mon_manager.update_info(mon_ds_cap);
+	
+	Genode::Xml_generator xml(_live_data.local_addr<char>(), _live_data.size(), "live", [&]()
+	{
+		xml.node("task-descriptions", [&]()
+		{
+					for (size_t i = 0; i < 100; i++) {
+						xml.node("task", [&]()
+						{
+			       			xml.attribute("id", std::to_string(threads[i].id).c_str());
+						xml.attribute("foc_id", std::to_string(threads[i].foc_id).c_str());
+						xml.attribute("execution-time", std::to_string(threads[i].execution_time.value/1000).c_str());
+			       			xml.attribute("priority", std::to_string(threads[i].prio).c_str());
+						xml.attribute("core", std::to_string(threads[i].affinity.xpos()).c_str());
+						xml.attribute("policy id", std::to_string(threads[i].policy_id.id).c_str());
+						xml.attribute("state", std::to_string(threads[i].state).c_str());
+						xml.attribute("arrival time", std::to_string(threads[i].arrival_time).c_str());
+						xml.attribute("start time", std::to_string(threads[i].start_time).c_str());
+						xml.attribute("session", threads[i].session_label.string());
+						xml.attribute("thread", threads[i].thread_name.string());
+			      	 		xml.attribute("ram_quota", std::to_string(threads[i].ram_quota/1024).c_str());
+						xml.attribute("ram_used", std::to_string(threads[i].ram_used/1024).c_str());
+						});
+					}
+			//Genode::printf("run %d\n",j);
+			_mon_manager.update_info(mon_ds_cap);
+		});
+	});
+	Genode::env()->ram_session()->free(mon_ds_cap);
+	return _live_data.cap();
+
+
+
+
+/*
+	Genode::printf("run parser!\n");
+
 	static Genode::Trace::Connection trace(1024*4096, 64*4096, 0);
 
 	Genode::Trace::Subject_id subjects[32];
 	size_t num_subjects = trace.subjects(subjects, 32);
-
+*/
 
 	//Task::log_profile_data(Task::Event::EXTERNAL, -1, _shared);
 
 	// Xml_generator directly writes XML data into the buffer on construction, explaining the heavy recursion here.
 	//PDBG("Generating event log. %d events have occurred.", _shared.event_log.size());
+/*
 	Genode::Xml_generator xml(_profile_data.local_addr<char>(), _profile_data.size(), "profile", [&]()
 	{
 		xml.node("task-descriptions", [&]()
@@ -93,6 +134,7 @@ Genode::Ram_dataspace_capability Parser_session_component::profile_data()
 				});
 			}
 		});
+*/
 
 		/*xml.node("events", [&]()
 		{
@@ -119,11 +161,11 @@ Genode::Ram_dataspace_capability Parser_session_component::profile_data()
 				});
 			}
 		});*/
-	});
+	//});
 
 	//_shared.event_log.clear();
 
-	return _profile_data.cap();
+	//return _profile_data.cap();
 }
 
 Genode::Number_of_bytes Parser_session_component::_profile_ds_size()
